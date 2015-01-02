5e9a6bc17e29b395446a1b9a3d073bc3a7cd2954
Mike Larsson <mikelarssonftw@gmail.com>
2015-01-02 11:01:52 -0500

    Start apps and redirect home to admin

--- attendance/settings.py
+++ attendance/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'households',
+    'programs',
+    'identifiers',
 )
 
 MIDDLEWARE_CLASSES = (
--- attendance/urls.py
+++ attendance/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+def see_admin(request, *args, **kwargs):
+    return HttpResponseRedirect('/admin')
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'attendance.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/?$', see_admin),
 )
