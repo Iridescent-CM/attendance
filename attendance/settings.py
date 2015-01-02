3713bf1178152da11ff5106e2c933fd68ce2841c
Mike Larsson <mikelarssonftw@gmail.com>
2015-01-02 16:33:46 -0500

    Getting heroku-ready

--- attendance/settings.py
+++ attendance/settings.py
@@ -8,23 +8,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*q4o0vpn6!5(0eni_vu+tist-j+#s1y)tcw)xcp)vc2q64!6pv'
+SECRET_KEY = os.getenv("SECRET_KEY", '*q4o0vpn6!5(0eni_vu+tist-j+#s1y)tcw)xcp)vc2q64!6pv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = TEMPLATE_DEBUG = 'DEBUG' in os.environ
 
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", '').split(',') if os.getenv("ALLOWED_HOSTS") else []
 
 
 # Application definition
@@ -60,10 +56,7 @@ WSGI_APPLICATION = 'attendance.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
 
 # Internationalization
@@ -84,3 +77,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Import optional local settings.  This must be at the END of this file.
+try:
+    from .local import *
+except ImportError:
+    pass
