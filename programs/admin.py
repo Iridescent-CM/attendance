0ebb58e88c70a9c9237120a6164b8a7201825d39
Mike Larsson <mikelarssonftw@gmail.com>
2015-01-15 13:27:23 -0500

    Better widget on both sides of session/student relationship

--- programs/admin.py
+++ programs/admin.py
@@ -1,3 +1,5 @@
+from django import forms
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib import admin
 from programs import models
 from households import models as households
@@ -11,9 +13,41 @@ class ProgramWithSessions(admin.ModelAdmin):
 class AttendeeInline(admin.TabularInline):
     model = households.Student.attended.through
 
+class SessionAdminForm(forms.ModelForm):
+    attendees = forms.ModelMultipleChoiceField(
+        queryset=households.Student.objects.all(),
+        required= False,
+        widget=FilteredSelectMultiple(
+            verbose_name='Students',
+            is_stacked=False
+        )
+    )
+
+    class Meta:
+        model = models.Session
+
+    def __init__(self, *args, **kwargs):
+        super(SessionAdminForm, self).__init__(*args, **kwargs)
+
+        if self.instance and self.instance.pk:
+            self.fields['attendees'].initial = self.instance.attendees.all()
+
+    def save(self, commit=True):
+        session = super(SessionAdminForm, self).save(commit=False)
+
+        if commit:
+            session.save()
+
+        if session.pk:
+            session.attendees = self.cleaned_data['attendees']
+            self.save_m2m()
+
+        return session
+
 class SessionAdmin(admin.ModelAdmin):
+    form = SessionAdminForm
+
     list_display = ('__str__', 'program', 'description', 'time', 'date')
-    inlines = [ AttendeeInline ]
 
 admin.site.register(models.Program, ProgramWithSessions)
 admin.site.register(models.Session, SessionAdmin)
